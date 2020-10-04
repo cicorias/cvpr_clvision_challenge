75c58a1d36e061a44a34e0a8cb5a53bf25fe97f3
Shawn Cicoria <github@cicoria.com>
2020-10-04 20:18:19 +0000

    addd normalize

--- continuum/baseline.py
+++ continuum/baseline.py
@@ -12,6 +12,7 @@ import torchvision.models as models
 from continuum import ClassIncremental
 from continuum.datasets import Core50
 from continuum.tasks import split_train_val
+from torchvision.transforms.transforms import Normalize, ToTensor
 
 
 ######################### UTIL FUNCTIONS ################################
@@ -86,13 +87,14 @@ class MyCore50(Core50):
 ####################### CONTINUOUS LEARNING IMPLEMENTATION ##############################
 
 # Load the core50 data
-core50 = Core50("core50/data/", train=True, download=True)
+core50 = Core50("core50/data/", train=True, download=False)
 
 # A new classes scenario
 scenario = ClassIncremental(
     core50,
     increment=5,
-    initial_increment=10
+    initial_increment=10,
+    transformations=[ ToTensor(), Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])]
 )
 
 print(f"Number of classes: {scenario.nb_classes}.")
